f36c56c0dd13189343a0edeee9dfb4dd69fb28bf
Dmytro Melnychuk <melnichukdv@gmail.com>
2017-05-06 21:35:57 +0200

    Change endpoint to MessageTemplate instead of outputChannel

--- src/main/java/com/amid/distributed_spring/DistributedSpringAppApplication.java
+++ src/main/java/com/amid/distributed_spring/DistributedSpringAppApplication.java
@@ -1,6 +1,5 @@
 package com.amid.distributed_spring;
 
-import com.amid.distributed_spring.service.PrinterService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -10,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
@@ -18,12 +18,10 @@ import org.springframework.messaging.support.MessageBuilder;
 @ImportResource("integration-context.xml")
 public class DistributedSpringAppApplication implements ApplicationRunner {
 
-    private final Logger log = Logger.getLogger(PrinterService.class);
+    private final Logger log = Logger.getLogger(DistributedSpringAppApplication.class);
 
     @Autowired
     private DirectChannel inboundChannel;
-    @Autowired
-    private DirectChannel outputChannel;
 
     public static void main(String[] args) {
         SpringApplication.run(DistributedSpringAppApplication.class, args);
@@ -31,17 +29,14 @@ public class DistributedSpringAppApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
-        outputChannel.subscribe(this::printMessage);
 
         Message<String> message = MessageBuilder
                 .withPayload("Let's integrate")
                 .setHeader("HeaderKey", "HeaderValue")
                 .build();
 
-        inboundChannel.send(message);
-    }
-
-    private void printMessage(Message<?> message) {
-        log.info(message.getPayload());
+        // the reply, possibly null if the message could not be received to destination
+        Message receivedMessage = new MessagingTemplate().sendAndReceive(inboundChannel, message);
+        log.info("MessageTemplate " + receivedMessage.getPayload());
     }
 }
