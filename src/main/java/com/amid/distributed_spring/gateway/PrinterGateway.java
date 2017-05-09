f50d4ce282e5439caccc190f227c1e1b8a5a4485
Dmytro Melnychuk <melnichukdv@gmail.com>
2017-05-10 01:35:05 +0200

    Improvement - remove inbound gateway return type as I didn't expect any result

--- src/main/java/com/amid/distributed_spring/gateway/PrinterGateway.java
+++ src/main/java/com/amid/distributed_spring/gateway/PrinterGateway.java
@@ -2,12 +2,10 @@ package com.amid.distributed_spring.gateway;
 
 import org.springframework.messaging.Message;
 
-import java.util.concurrent.Future;
-
 /**
  * @author Dmytro Melnychuk
  */
 public interface PrinterGateway {
 
-    Future<Message<String>> print(Message<?> message);
+    void print(Message<?> message);
 }
