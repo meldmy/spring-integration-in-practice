190c924b95cada8323a7ac29465f4d4531b88c5d
Dmytro Melnychuk <melnichukdv@gmail.com>
2017-05-23 00:55:31 +0200

    Add RandomClientGenerator for creation random asset product

---
+++ src/main/java/com/amid/distributed_spring/generator/RandomClientGenerator.java
@@ -0,0 +1,30 @@
+package com.amid.distributed_spring.generator;
+
+import com.amid.distributed_spring.entity.BankClient;
+import com.google.common.io.BaseEncoding;
+
+import java.util.Random;
+
+/**
+ * @author Dmytro Melnychuk
+ */
+class RandomClientGenerator {
+    private static final int MAX_ID = 1_000_500;
+    private static final int NAME_LENGTH = 7;
+    private static final int SURNAME_LENGTH = 13;
+    private final Random random = new Random();
+
+    BankClient generate() {
+        return new BankClient(randomId(), randomAlphanumeric(NAME_LENGTH), randomAlphanumeric(SURNAME_LENGTH));
+    }
+
+    private String randomAlphanumeric(int length) {
+        final byte[] buffer = new byte[length];
+        random.nextBytes(buffer);
+        return BaseEncoding.base64Url().omitPadding().encode(buffer);
+    }
+
+    private int randomId() {
+        return random.nextInt(MAX_ID);
+    }
+}
